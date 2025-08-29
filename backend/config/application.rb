bcbe660526aca71019b0657745cb82ad51ecc808
MatthieuGriffon <postmaster@matthieu-griffon.fr>
2025-08-30 01:37:46 +0200

    feat(auth): setup Devise with User model using UUID and roles
    - Installed and configured Devise
    - Generated User model with Devise authentication
    - Configured database to use UUID as primary keys
    - Added role column with enum { user, editor, admin }
    - Verified User creation in Rails console and PostgreSQL

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -28,5 +28,10 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # 👇 Ajout pour que toutes les PK soient en UUID par défaut
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
